fc4d0ac6b09ba9fbb1098b18a9c2c18bfd0ed1ba
chairil ashar <asharchairil4@gmail.com>
2023-01-27 14:30:21 +0700

    storing product

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('63d3354e4051a6651b30acb1')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
